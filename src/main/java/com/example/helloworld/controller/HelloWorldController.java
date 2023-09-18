c9e4dbee1c7c7db63bb0b45c207451804929f159
rubinoluca <145401902+rubinoluca@users.noreply.github.com>
2023-09-19 00:58:23 +0200

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -2,6 +2,8 @@ package com.example.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class HelloWorldController {
