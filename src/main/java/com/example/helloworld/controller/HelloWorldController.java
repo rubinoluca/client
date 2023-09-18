3356426c3447c307538098903ed11f249b7976b3
rubinoluca <145401902+rubinoluca@users.noreply.github.com>
2023-09-19 00:55:06 +0200

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -6,14 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
     @GetMapping("/")
-    private static void getStudents()
+    public String getStudents()
     {
         final String uri = "http://10.217.4.86/students";
     
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
-    
-        System.out.println(result);
+        return result;
     }
     @GetMapping("/hello")
     public String sendGreetings() {
