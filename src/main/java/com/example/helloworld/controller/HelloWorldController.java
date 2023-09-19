9b801249ad8de78191444aa4dbe34bc45204366b
Luru99 <lu.rubino@reply.it>
2023-09-19 13:16:40 +0200

    up

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 
 @RestController
 public class HelloWorldController {
@@ -20,7 +23,10 @@ public class HelloWorldController {
             return result;
         }
         catch (Exception e){
-            return result;
+            StringWriter errors = new StringWriter();
+            e.printStackTrace(new PrintWriter(errors));
+            return errors.toString();
+
         }
 
     }
