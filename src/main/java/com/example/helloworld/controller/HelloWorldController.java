07281ddd6ce5945be996cdbf93f3017aa2df7146
rubinoluca <145401902+rubinoluca@users.noreply.github.com>
2023-09-19 09:57:08 +0200

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
     @GetMapping("/")
     public String getStudents()
     {
-        final String uri = "https://10.217.4.86/students";
+        final String uri = "https://10.217.4.86/hello";
     
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, String.class);
