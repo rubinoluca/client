7d4d65e246a263b6f1238d3f1db8452774c82451
rubinoluca <145401902+rubinoluca@users.noreply.github.com>
2023-10-02 10:44:46 +0200

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -49,6 +49,6 @@ public class HelloWorldController {
 
     @GetMapping("/")
     public String sendGreetings() {
-        return "Il consumer funziona. MODIFICATO";
+        return "Il consumer funziona. MODIFICATO 2";
     }
 }
